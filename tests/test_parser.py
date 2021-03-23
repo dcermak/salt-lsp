e02c55a102c3375d0c96571692c78a2d4e3b5d7d
Dan Čermák <dcermak@suse.com>
2021-03-23 13:47:35 +0100

    [server] Add more tests for construct_path_to_position

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -20,9 +20,13 @@ git -C /srv/salt pull -q:
   cron.present:
     - user: root
     - minute: '*/5'
+    - dummy:
+      -
+    -
 
 /srv/git/salt-states:
   file.symlink:
+    -
     - target: /srv/salt
 
 /etc/systemd/system/rootco-salt-backup.service:
@@ -77,4 +81,32 @@ class TestPathToPosition:
             "pkg.installed",
             1,
             "require",
+        ]
+
+    def test_path_to_dummy(self):
+        assert construct_path_to_position(
+            SLS_PARSED, Position(line=14, character=7)
+        ) == [
+            "git -C /srv/salt pull -q",
+            "cron.present",
+            2,
+            "dummy",
+        ]
+
+    def test_path_after_dummy(self):
+        assert construct_path_to_position(
+            SLS_PARSED, Position(line=15, character=5)
+        ) == [
+            "git -C /srv/salt pull -q",
+            "cron.present",
+        ]
+
+    def test_path_before_target(self):
+        assert construct_path_to_position(
+            SLS_PARSED,
+            # before "target: /srv/salt"
+            Position(line=19, character=5),
+        ) == [
+            "/srv/git/salt-states",
+            "file.symlink",
         ]
