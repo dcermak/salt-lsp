7d7596a4196b938ef4e9b49e063015cb1e273323
Dan Čermák <dcermak@suse.com>
2021-03-23 16:19:47 +0100

    Add script to dump completion classes to disk

---
+++ salt_lsp/create_completion_classes.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+import subprocess
+import shlex
+import json
+import pickle
+import tempfile
+import os
+from typing import Any, Dict, List
+
+
+class StateNameCompletion:
+    def __init__(self, state_name: str, state_params: List[Any]) -> None:
+        self.state_name = state_name
+        self.state_params = {}
+
+        for submod in state_params:
+            submod_name = next(iter(submod.keys()))
+            self.state_params[submod_name] = submod[submod_name]
+
+        self.state_sub_names = list(self.state_params.keys())
+
+    def provide_subname_completion(self) -> List[str]:
+        return self.state_sub_names
+
+    def provide_param_completion(self, submod_name: str) -> List[str]:
+        return list(self.state_params[submod_name].keys())
+
+
+state_completions: Dict[str, StateNameCompletion] = {}
+
+
+with tempfile.TemporaryDirectory() as tmpdirname:
+    salt_dest = os.path.join(tmpdirname, "salt")
+    minion_conf_file = os.path.join(salt_dest, "minion")
+    os.mkdir(salt_dest)
+    with open(minion_conf_file, "w") as minion_file:
+        minion_file.write(f"root_dir: {tmpdirname}")
+
+    docs = json.loads(
+        str(
+            subprocess.run(
+                shlex.split(
+                    f"salt-call --local -c {salt_dest} --out json "
+                    "baredoc.list_states"
+                ),
+                capture_output=True,
+            ).stdout,
+            encoding="utf-8",
+        )
+    )
+
+    for module in docs["local"]:
+        state_completions[module] = StateNameCompletion(
+            module, docs["local"][module]
+        )
+
+
+with open(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), "states.pickle"),
+    "wb",
+) as states_file:
+    pickle.dump(state_completions, states_file)
