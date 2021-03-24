94a2ccb41588aeff8d51fdc83c87ce87e1b15b4e
Dan Čermák <dcermak@suse.com>
2021-03-24 20:51:44 +0100

    Make the logging level tweakable via the CLI

--- lsp_server.py
+++ lsp_server.py
@@ -4,12 +4,25 @@ import argparse
 import logging
 import pickle
 from os.path import dirname, abspath, join
+from typing import Dict
 
 from salt_lsp.server import salt_server
 
-logging.basicConfig(
-    filename="salt-server.log", level=logging.DEBUG, filemode="w"
-)
+LOG_LEVEL_DICT: Dict[str, int] = {
+    "critical": logging.CRITICAL,
+    "fatal": logging.FATAL,
+    "error": logging.ERROR,
+    "warning": logging.WARNING,
+    "warn": logging.WARN,
+    "info": logging.INFO,
+    "debug": logging.DEBUG,
+}
+
+
+def loglevel_from_str(level: str) -> int:
+    if level.lower() not in LOG_LEVEL_DICT:
+        return logging.DEBUG
+    return LOG_LEVEL_DICT[level.lower()]
 
 
 def add_arguments(parser):
@@ -30,6 +43,14 @@ def add_arguments(parser):
         help="initialize the server, but don't launch it "
         "(useful for debugging/testing purposes)",
     )
+    parser.add_argument(
+        "--log-level",
+        choices=list(LOG_LEVEL_DICT.keys())
+        + list(map(lambda level: level.upper(), LOG_LEVEL_DICT.keys())),
+        default=["debug"],
+        nargs=1,
+        help="Logging verbosity",
+    )
 
 
 def main():
@@ -37,11 +58,18 @@ def main():
     add_arguments(parser)
     args = parser.parse_args()
 
+    log_level = loglevel_from_str(args.log_level[0])
+    logging.basicConfig(
+        filename="salt-server.log",
+        level=log_level,
+        filemode="w",
+    )
+
     with open(
         join(dirname(abspath(__file__)), "data", "states.pickle"), "rb"
     ) as states_file:
         states = pickle.load(states_file)
-    salt_server.post_init(states)
+    salt_server.post_init(states, log_level)
 
     if args.stop_after_init:
         return
